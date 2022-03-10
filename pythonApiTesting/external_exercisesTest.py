05d842e40b33f42a5aaea74f4fa6e0363e749a4a
Louis Doherty <lousterrd@gmail.com>
2022-03-09 22:24:43 -0800

    cleaning up code

--- pythonApiTesting/external_exercisesTest.py
+++ pythonApiTesting/external_exercisesTest.py
@@ -1,6 +1,6 @@
 import requests
 
-api_host = "https://rockFIITapi.lousterrd.repl.co"
+api_host = 'https://rockFIITapi.lousterrd.repl.co'
 
 path = '/exercises'
 response = requests.get(api_host + path)
@@ -9,4 +9,4 @@ print(response.status_code)
 try:
     print(response.json())
 except:
-    print("Response not json")
+    print('Response not json')
